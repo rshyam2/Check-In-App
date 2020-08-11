9703de898d9f8325246ddf1da5caba254fdc907b
Radhe Shyam Pokhrel <52668053+rshyam2@users.noreply.github.com>
2020-08-11 15:29:26 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
-# File that serves as an entry point for the scripts
-from scripts import interface
+from scripts import interface, log_generator
+import threading
+
 if __name__ == '__main__':
-    interface.runtime()
+    runner = False
+    thread1 = threading.Thread(target=interface.runtime, args=())
+    thread2 = threading.Thread(target=log_generator.port, args=(), daemon=True)
+    thread1.start()
+    thread2.start()
+    thread1.join()
+
